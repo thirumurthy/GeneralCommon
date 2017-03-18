d708fb59b8820895658a226069db4edd8a9ccdf0
Thirumurthy <91thiru@gmail.com>
2017-03-18 11:35:07 +0530

    initial

---
+++ src/Common/FileOperations.java
@@ -0,0 +1,54 @@
+package Common;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileOperations {
+	
+	public static String readFile(String filename)
+	{
+		String resp="";
+		try (BufferedReader br = new BufferedReader(new FileReader(filename)))
+		{
+
+			String sCurrentLine;
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				resp+=sCurrentLine;
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+
+		return resp;
+	}
+
+	public static void WriteData(String filename,String content)
+	{
+		try {
+ 
+			File file = new File(filename);
+
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+ 
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
